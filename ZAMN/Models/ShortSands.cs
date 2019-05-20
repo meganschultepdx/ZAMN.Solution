d762f45b5816abc914485ce43855ffcbbc0ff9c6
AJ Ancheta <ashleyjancheta@gmail.com>
2019-05-20 16:00:16 -0700

    add changes to shortsands model

--- ZAMN/Models/ShortSands.cs
+++ ZAMN/Models/ShortSands.cs
@@ -14,9 +14,35 @@ namespace ZAMN.Models
       _id = id;
     }
 
+    public string Name { get => _name; set => _name = value; }
+
     public int GetId()
     {
       return _id;
     }
+
+    public static List<ShortSands> GetAll()
+    {
+      List<ShortSands> allShortSands = new List<ShortSands>{};
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      var cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"SELECT * FROM stylists;";
+      var rdr = cmd.ExecuteReader() as MySqlDataReader;
+      while(rdr.Read())
+      {
+        int stylistId = rdr.GetInt32(0);
+        string stylistName = rdr.GetString(1);
+        ShortSands newShortSands = new ShortSands(stylistName, stylistId);
+        allShortSands.Add(newShortSands);
+      }
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+      return allShortSands;
+    }
+
   }
 }
