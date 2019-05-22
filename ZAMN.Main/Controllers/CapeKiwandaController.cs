11b8e8622edb2f304085832189c4f48ae1c56a29
megan schulte <megan.schulte.pdx@gmail.com>
2019-05-22 08:27:40 -0700

    remove location controllers

--- ZAMN.Main/Controllers/CapeKiwandaController.cs
+++ ZAMN.Main/Controllers/CapeKiwandaController.cs
@@ -14,36 +14,6 @@ namespace ZAMN.Controllers
       return View();
     }
 
-    [HttpGet("/capeKiwanda/new")]
-    public ActionResult New()
-    {
-      return View();
-    }
 
-    [HttpPost("/capeKiwanda")]
-    public ActionResult Create(string userName, string userComment)
-    {
-      SurfLocation newComment = new SurfLocation(userName, userComment);
-      newComment.Save();
-
-      List<SurfLocation> allComments = SurfLocation.GetAll();
-
-      return View("Index", allComments);
-    }
-
-    // [HttpGet("/capeKiwanda/{id}")]
-    // public ActionResult Show(int id)
-    // {
-    //   Dictionary<string, object> model = new Dictionary<string, object> ();
-    //   SurfLocation selectedSurfLocation = SurfLocation.Find(id);
-    //   List<User> allUserComments = User.GetAll();
-    //   List<User> locationCommentsByUser = selectedSurfLocation.GetUsers();
-    //
-    //   model.Add("surfLocation", selectedSurfLocation);
-    //   model.Add("allUserComments", allUserComments);
-    //   model.Add("locationCommentsByUser", locationCommentsByUser);
-    //
-    //   return View(model);
-    // }
   }
 }
--- ZAMN.Main/Controllers/ShortSands.cs
+++ ZAMN.Main/Controllers/ShortSands.cs
@@ -12,44 +12,5 @@ namespace ZAMN.Controllers
       return View();
     }
 
-    // [HttpGet("/shortSands/user/new")]
-    // public ActionResult New()
-    // {
-    //   return View();
-    // }
-    //
-    // [HttpPost("/shortSands/{shortSandsId}/users")]
-    // public ActionResult Create(int shortSandsId, string shortSandsName, string comment, int id)
-    // {
-    //   Dictionary<string, object> model = new Dictionary<string, object>();
-    //   ShortSands foundShortSands = ShortSands.Find(shortSandsId);
-    //   User newUser = new User(userName, userComment, shortSandsId);
-    //   newUser.Save();
-    //   List<Comments> allComments = Comments.GetAll();
-    //   ShortSand selectedShortSand = ShortSand.Find(id);
-    //   List<Comment> shortSandComments = selectedShortSand.GetComments();
-    //   List<User> shortSandUsers = foundShortSand.GetUsers();
-    //   model.Add("clients", shortSandUsers);
-    //   model.Add("shortSand", foundShortSand);
-    //   model.Add("allComments", allComments);
-    //   model.Add("shortSandComments", shortSandComments);
-    //   return View("Show", model);
-    // }
-    //
-    // [HttpGet("/shortSands/{shortSandsId}/users/{userId}")]
-    // public ActionResult Show(int id)
-    // {
-    //   Dictionary<string, object> model = new Dictionary<string, object>();
-    //   ShortSand selectedShortSand = ShortSand.Find(id);
-    //   List<User> shortSandUsers = selectedShortSand.GetUsers();
-    //   List<Comment> shortSandComments = selectedShortSand.GetComments();
-    //   List<Comment> allComments = Comment.GetAll();
-    //   model.Add("shortSand", selectedShortSand);
-    //   model.Add("clients", shortSandUsers);
-    //   model.Add("shortSandComments", shortSandComments);
-    //   model.Add("allComments", allComments);
-    //   return RedirectToAction("Index", "shortSands");
-    // }
-
   }
 }
